eedcb6fed4ee0cd7d34f4348997b22493e5f8137
安本 涼 <ae24108@shibaura-it.ac.jp>
2026-03-19 22:02:13 +0900

    prevented duplicate submission

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,24 @@ function closeDeleteModal(){
     const modal = document.getElementById("deleteModal");
     modal.style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const form = document.getElementById("reviewForm");
+    const submitButton = document.getElementById("submitButton");
+
+    if(!form) return;
+
+    form.addEventListener("submit", function(event){
+        if (form.dataset.submitted == "true") {
+            event.preventDefault();
+            return;
+        }
+
+        form.dataset.submitted = "true"
+
+        if (submitButton) {
+            submitButton.disable = true;
+            submitButton.textContent = "送信中…";
+        }
+    });
+});
